98d84a701e2d40bfaae68ac5bb28a76a3c3e6085
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2021-08-31 12:04:59 -0400

    promediador con ponderacion
    grilla, input # funcion

--- src/app/pagina3/pagina3.page.ts
+++ src/app/pagina3/pagina3.page.ts
@@ -7,15 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Pagina3Page implements OnInit {
 // atributos
+
+  promedio : number;
   constructor() { }
   ngOnInit() {
   }
 // metodos
-  onClick(n1 : HTMLInputElement, n2 :HTMLInputElement, n3 :HTMLInputElement)
+  onClick(n1 : HTMLInputElement, n2 :HTMLInputElement, n3 :HTMLInputElement,
+          p1 : HTMLInputElement, p2 :HTMLInputElement, p3 :HTMLInputElement)
   {
-    let res = Number(n1.value)+Number(n2.value)+Number(n3.value)
-    res = res/ 3
-    console.log("El promedio es: " + res)
+    /*
+    let res = Number(n1.value)+Number(n2.value)+Number(n3.value);
+    res = res/ 3;
+    this.promedio = res;
+    console.log("El promedio es: " + res);
+    */
+    let no1 = Number(n1.value) * Number(p1.value)/ 100
+    let no2 = Number(n2.value) * Number(p2.value)/ 100
+    let no3 = Number(n3.value) * Number(p3.value)/ 100
+
+    this.promedio = no1 + no2 + no3
   }
 
 
